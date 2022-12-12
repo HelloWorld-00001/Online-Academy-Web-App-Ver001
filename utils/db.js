ff33150aa1742f42bc1755c38b9e664abe6933b1
lumifa <phatluu0867571874@gmail.com>
2022-12-12 14:36:25 +0700

    update gitignore

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ const knex = knexObj({
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: '123451',
+        password: 'root',
         database: 'db_course'
     },
     pool: { min: 0, max: 10 }
