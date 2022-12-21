ebbb119d7ffa2cf5e50b58a92359c9f60670426b
PhiHung1503 <phihung15032001@gmail.com>
2022-12-21 16:49:33 +0700

    create form review BE post

--- routes/mylearning-course.route.js
+++ routes/mylearning-course.route.js
@@ -3,26 +3,12 @@ import courseService from "../services/course.service.js";
 import express from "express";
 const router = express.Router();
 
-router.post('/id', async function (req, res) {
-    let makhoahoc = req.params.id || 0;
-    const oneStarRate = await mylearningService.countStarRate(makhoahoc,1);
-    const twoStarRate = await mylearningService.countStarRate(makhoahoc,2);
-    const threeStarRate = await mylearningService.countStarRate(makhoahoc,3);
-    const fourStarRate = await mylearningService.countStarRate(makhoahoc,4);
-    const fiveStarRate = await mylearningService.countStarRate(makhoahoc,5);
-
-    const amountStudentRating = await mylearningService.countStudentRating(makhoahoc);
-    const sumRating = oneStarRate + twoStarRate + threeStarRate + fourStarRate + fiveStarRate;
-    const totalRate = await  mylearningService.sumStudentRating(makhoahoc);
-
-    const RateTB = Math.round(totalRate / amountStudentRating *10) / 10  || 0;
-    const ret = await mylearningService.addRating(makhoahoc, RateTB, amountStudentRating);
-    res.render('vwMylearning/mylearning',);
-});
 
 router.get('/:id', async function (req, res) {
     let makhoahoc = req.params.id || 0;
+    // console.log(makhoahoc)
     const course = await courseService.findDetailCourseByID(makhoahoc);
+    const inforStudentsOfTeacher = await courseService.inforStudentsOfTeacher(course.GiaoVien);
 
     if (course === null) {
         return res.redirect('/');
@@ -49,14 +35,70 @@ router.get('/:id', async function (req, res) {
     const fiveStarRatePer = Math.round(fiveStarRate / amountStudentRating * 100)
 
     const starRatingList  = {oneStarRatePer, twoStarRatePer, threeStarRatePer, fourStarRatePer, fiveStarRatePer};
-console.log(starRatingList)
     const firstVideo = courseVideoList[0].Link;
-    console.log(firstVideo)
     res.render('vwMylearning/mylearning', {
         courseVideoList,
         course,
         firstVideo,
         starRatingList,
+        inforStudentsOfTeacher,
     });
 });
+
+router.post('/:id', async function (req, res) {
+    let makhoahoc1 = req.params.id || 0;
+    const course = await courseService.findDetailCourseByID(makhoahoc1);
+    const inforStudentsOfTeacher = await courseService.inforStudentsOfTeacher(course.GiaoVien);
+
+    if (course === null) {
+        return res.redirect('/');
+    }
+
+    const result = req.body;
+    const data = {
+        'MaHocVien': 2,
+        'MaKhoaHoc': makhoahoc1,
+        'Rate': result.Rate,
+        'Comment': result.Comment,
+    }
+
+    const ret1 = await mylearningService.addBangDanhGia(data);
+
+    const courseVideoList = await courseService.findCourseVideoList(makhoahoc1);
+    const oneStarRate = await mylearningService.countStarRate(makhoahoc1,1);
+    const twoStarRate = await mylearningService.countStarRate(makhoahoc1,2);
+    const threeStarRate = await mylearningService.countStarRate(makhoahoc1,3);
+    const fourStarRate = await mylearningService.countStarRate(makhoahoc1,4);
+    const fiveStarRate = await mylearningService.countStarRate(makhoahoc1,5);
+
+    const amountStudentRating = await mylearningService.countStudentRating(makhoahoc1);
+
+    const oneStarRatePer = Math.round(oneStarRate / amountStudentRating * 100)
+    const twoStarRatePer = Math.round(twoStarRate / amountStudentRating * 100)
+    const threeStarRatePer = Math.round(threeStarRate / amountStudentRating * 100)
+    const fourStarRatePer = Math.round(fourStarRate / amountStudentRating * 100)
+    const fiveStarRatePer = Math.round(fiveStarRate / amountStudentRating * 100)
+
+    const starRatingList  = {oneStarRatePer, twoStarRatePer, threeStarRatePer, fourStarRatePer, fiveStarRatePer};
+    const firstVideo = courseVideoList[0].Link;
+
+    const sumRating = oneStarRate + twoStarRate + threeStarRate + fourStarRate + fiveStarRate;
+    const totalRate = await  mylearningService.sumStudentRating(makhoahoc1);
+    console.log({totalRate, amountStudentRating})
+    const RateTB = Math.round(totalRate / amountStudentRating *10) / 10  || 0;
+
+
+    console.log({RateTB, amountStudentRating})
+    const ret2 = await mylearningService.updateKhoaHoc(makhoahoc1, RateTB, amountStudentRating);
+
+    res.render('vwMylearning/mylearning', {
+        courseVideoList,
+        course,
+        firstVideo,
+        starRatingList,
+        inforStudentsOfTeacher,
+    });
+});
+
+
 export default router;
--- services/myleaning.service.js
+++ services/myleaning.service.js
@@ -2,37 +2,46 @@ import db from "../utils/db.js";
 
 export default {
     async countStarRate(idCourse, numberRate) {
-        const numberStar= await db('Khoahoc')
-            .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
-            .where('khoahoc.MaKhoaHoc', idCourse)
-            .where('bangdanhgia.Rate', numberRate)
-            .count({amount : 'bangdanhgia.MaHocVien'});
+        const numberStar= await db('bangdanhgia')
+            // .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
+            .where('MaKhoaHoc', idCourse)
+            .where('Rate', numberRate)
+            .count({amount : 'MaHocVien'});
 
         return numberStar[0].amount;
     },
 
     async countStudentRating(idCourse) {
-        const numberStar= await db('Khoahoc')
-            .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
-            .where('khoahoc.MaKhoaHoc', idCourse)
-            .count({amount : 'bangdanhgia.MaHocVien'});
+        const numberStar= await db('bangdanhgia')
+            // .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
+            .where('MaKhoaHoc', idCourse)
+            .count({amount : 'MaHocVien'});
 
         return numberStar[0].amount;
     },
 
     async sumStudentRating(idCourse) {
-        const totalRate= await db('Khoahoc')
-            .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
-            .where('khoahoc.MaKhoaHoc', idCourse)
-            .sum({total : 'bangdanhgia.Rate'});
+        const totalRate= await db('bangdanhgia')
+            // .innerJoin('bangdanhgia', {'khoahoc.MaKhoaHoc': 'bangdanhgia.MaKhoaHoc'})
+            .where('MaKhoaHoc', idCourse)
+            .sum({total : 'Rate'});
 
         return totalRate[0].total;
     },
 
-    addRating(idCourse, rateTB, amountStudentRating) {
+    // addRating(idCourse, rateTB, amountStudentRating) {
+    //     return db('khoahoc')
+    //         .where('MaKhoaHoc', idCourse)
+    //         .update({ RateTB: rateTB, SLHocSinhDanhGia: amountStudentRating })
+    //
+    // },
+    addBangDanhGia(newRating) {
+        return db('bangdanhgia').insert(newRating);
+    },
+
+    updateKhoaHoc(idCourse, rateTB, amountStudentRating) {
         return db('khoahoc')
             .where('MaKhoaHoc', idCourse)
             .update({ RateTB: rateTB, SLHocSinhDanhGia: amountStudentRating })
-
     },
 }
